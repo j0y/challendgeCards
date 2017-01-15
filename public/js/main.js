7a0ab63c17b1d4f8786e833b975f9cc943d8a10d
j0y <hope2013@mail.ru>
2017-01-15 18:34:20 +0700

    Google maps

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,17 @@
  * Main AngularJS Web Application
  */
 var app = angular.module('WebApp', [
-  'ngRoute'
+  'ngRoute',
+  'uiGmapgoogle-maps'
 ]);
+
+app.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyBqoa1ZLYZ-za9w8KxhZlWVcNxtP_J0Ni8',
+        v: '3.26', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+});
 
 /**
  * Configure the Routes
@@ -13,17 +22,54 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl"})
     // Pages
-    .when("/track", {templateUrl: "partials/track.html", controller: "TrackCtrl"})
+    .when("/checkin", {templateUrl: "partials/checkin.html", controller: "CheckinCtrl"})
     .when("/create", {templateUrl: "partials/create.html", controller: "CreateCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html"});
 }]);
 
 /**
- * Controls the card tracking
+ * Controls the card checkining
  */
-app.controller('TrackCtrl', function (/* $scope, $location, $http */) {
-  console.log("Track Controller reporting for duty.");
+app.controller('CheckinCtrl', function ($scope, uiGmapGoogleMapApi) {
+  console.log("Checkin Controller reporting for duty.");
+
+  $scope.checkin = function() {
+      console.log($scope.trackID);
+      console.log($scope.comment);
+      console.log($scope.map.marker);
+  };
+
+  //set initial marker value
+  uiGmapGoogleMapApi.then(function(maps) {
+    $scope.map.marker = { id: Date.now() };
+  });
+
+  angular.extend($scope, {
+        map: {
+            center: {
+                latitude: 55.3435941,
+                longitude: 86.0614013
+            },
+            zoom: 11,
+            streetViewControl: false,
+            events: {
+            click: function (map, eventName, originalEventArgs) {
+                var e = originalEventArgs[0];
+                var lat = e.latLng.lat(),lon = e.latLng.lng();
+                var marker = {
+                    id: Date.now(),
+                    coords: {
+                        latitude: lat,
+                        longitude: lon
+                    }
+                };
+                $scope.map.marker = marker;
+                $scope.$apply();
+            }
+        }
+        }
+      });
 });
 
 /**
